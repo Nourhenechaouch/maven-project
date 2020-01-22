89de5334919d57a87ec60454f841cc54401c1ae4
User <nourhene.chaouch25@gmail.com>
2020-01-22 12:06:36 +0100

    fixing

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,10 +12,10 @@ public class Greeter {
 
   }
   
-/**
- *@param someone name of person
- *@return greet string
- */
+  /**
+   * @param someone name of person
+   * @return greet string
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
